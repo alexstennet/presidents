f4b29bbc20a731557af91747f98d1dc82022c6de
Aviroop Sen <senavi@berkeley.edu>
2017-12-20 19:59:40 -0800

    more accidental work on master

--- sp_presidents.py
+++ sp_presidents.py
@@ -217,9 +217,8 @@ class PresidentsPlayer(Player):
         # the card before the passes
         # if there are 3 passes, all players have passed and the current player is allowed
         # to play any hand
-
         elif isinstance(top_of_played, PresidentsPass):
-            return
+            
 
 
 
@@ -513,7 +512,6 @@ class Presidents:
     value_order = ['2', '3', '4', '5', '6', '7', '8', '9', 'j', 'q', 'k', 'a', '1']
     hand = PresidentsHand
     
-
     def __init__(self):
         # president's card deck, with cards ordered from weakest to strongest
         self.deck = Deck('Presidents',
@@ -547,32 +545,20 @@ class Presidents:
                 self.current_player = player
         print(f'{self.current_player.name} has the 3 of Clubs!')
         
-
-
-
     def report_turn(self):
         print(f"It's your turn, {self.current_player}!")
 
-    def game_loop(self):
-        return
-
-    
-
-    def __repr__(self):
-        return 'Presidents Game Instance'
-
-    # setup table for a game of pres, put start in the played list, shuffle and distribute
-    # cards, etc.a
     def setup_table(self):
         self.table.shuffle_deck()
         self.table.deal_cards()
         self.table.played.append(PresidentsStart())
-        
 
+    def game_loop(self):
+        return
 
-    # manage turns
+    def __repr__(self):
+        return 'Presidents Game Instance'
 
-    #
 
 class PresidentsStart:
     """
@@ -580,6 +566,7 @@ class PresidentsStart:
     """
     def __repr__(self):
         return 'Start'
+
 
 class PresidentsPass:
     """
