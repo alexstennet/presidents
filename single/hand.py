ed68d9e53920104a3d10893e062daecd345873c0
Avi Sen <senavi@berkeley.edu>
2018-04-06 17:30:56 -0700

    added remove and fixed some typing

--- single/hand.py
+++ single/hand.py
@@ -2,6 +2,7 @@ import numpy as np
 import deepdish as dd
 
 from helpers import hand_hash
+from typing import Union
 
 # hash table for identifying hands
 hand_table = dd.io.load("hand_table.h5")
@@ -33,7 +34,20 @@ class Hand:
     """
     Base class for president's hands and the core data structure of
     Presidents. Refactoring from original Hand class found in class
-    Hand. Uses numpy arrays to represent hands.
+    Hand within spcl_presidents.py. Uses numpy arrays to represent
+    hands.
+
+    Right now, the way this is set up is for individual selection of
+    cards in a gui where the player would click on a card to start
+    building a hand and have the validity (whether it is a double, 
+    triple, bomb, etc.) of the hand update dynamically.
+
+    There will always be an empty hand and that hand can be put into 
+    hand storage if it is a valid non single hand; once a hand is put
+    into storage, a new empty hand will be created. Singles cannot be
+    stored. If a single card is selected, all stored hands containing
+    that card will be highlighted, signifying deletion of the stored
+    hand if the single is played.
     """
 
     # cards are numbered 1-52
@@ -57,57 +71,50 @@ class Hand:
 
     def __init__(self, initial_card: int) -> None:
         """
-        Must be initialized with initial card, i.e. cannot make an empty
-        hand.
-
-        Right now, the way this is set up is for individual selection of
-        cards in a gui where the player would click on a card to start
-        building a hand and have the validity (whether it is a double, 
-        triple, bomb, etc.) of the hand update dynamically.
-
-        If the player unselects all cards, the empty hand should not
-        persist.
+        Hands always start out empty.
         """
         self._cards = np.zeros(shape=5, dtype=np.uint8)
-        self._id = 11
-        if initial_card:
-            self[4] = initial_card
+        self._id = 0
+        self._insertion_index = 4
 
-    def __getitem__(self, key: int) -> int:
+    def __getitem__(self, key: Union[int, slice]) -> int:
         return self._cards[key]
 
-    def __setitem__(self, key: int, value: int) -> None:
+    def __setitem__(self, key: Union[int, slice], value: int) -> None:
         self._cards[key] = value
+
+    def __contains__(self, value: int) -> bool:
+        return value in self._cards
 
     @property
     def _is_full(self) -> bool:
-        return self[0] != 0
+        return self._insertion_index == -1
 
     @property
-    def _insertion_index(self) -> int:
-        return 4 - self._id // 10
+    def _number_of_cards(self) -> int:
+        return 4 - self._insertion_index
 
     def _identify(self) -> None:
-        hh = hash(self)
-        if hh not in hand_table:
-            num_cards = 5 - self._insertion_index
-            self._id = invalid_id_dict[num_cards]
+        h = hash(self)
+        if h not in hand_table:
+            self._id = invalid_id_dict[self._number_of_cards]
         else:
-            self._id = hand_table[hh]
+            self._id = hand_table[h]
 
     def _add(self, card: int) -> None:
         assert 1 <= card <= 52, "Bug: attempting to add invalid card."
-        assert card not in self._cards, "Bug: attemping to add duplicate card."
+        assert card not in self, "Bug: attemping to add duplicate card."
         if (self._is_full):
-            print("Hand is full.")
+            print("Cannot add any more cards to this hand.")
             return
         else:
             ii = self._insertion_index
             self[ii] = card
-            self._sort(ii)
+            self._add_sort(ii)
+            self._insertion_index -= 1
         self._identify()
 
-    def _sort(self, index: int) -> None:
+    def _add_sort(self, index: int) -> None:
         """
         Special sorting algorithm -- wow v algorithm
         runs in big omega 1, big o 4
@@ -123,9 +130,27 @@ class Hand:
             return
         elif self[i] > self[i + 1]:  # inserted card is greater than next
             self[i], self[i + 1] = self[i + 1], self[i]  # pythonic swap
-            self._sort(i + 1)  # recurse on inserted card's new position
+            self._add_sort(i + 1)  # recurse on inserted card's new position
         else:  # inserted card is less than next, i.e. in the right position
             return
+
+    def _card_index(self, card) -> int:
+        assert (card in self,
+                "Bug: attempting to find index of card which is not in hand.")
+        return np.where(self._cards == card)[0][0]
+
+    def _remove(self, card) -> None:
+        assert self._id != 0, "Bug: attempting to remove from an empty hand."
+        ci = self._card_index(card)
+        self[ci] = 0
+        self._insertion_index += 1
+        self._remove_sort(ci)
+
+    def _remove_sort(self, card_index: int) -> None:
+        ci = card_index
+        ii = self._insertion_index
+        self[ii + 1: ci + 1] = self[ii: ci]
+        self[ii] = 0
 
     @property
     def _id_desc(self) -> str:
